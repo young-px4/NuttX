0a28504d4a365d2d4806f6cdf3deffc7849aab3a
Daniel Agar <daniel@agar.ca>
2019-07-08 21:26:22 -0400

    WIP: backport dcache_invalidate

--- arch/arm/src/armv7-m/arch_invalidate_dcache.c
+++ arch/arm/src/armv7-m/arch_invalidate_dcache.c
@@ -80,61 +80,37 @@
 void arch_invalidate_dcache(uintptr_t start, uintptr_t end)
 {
   uint32_t ccsidr;
-  uint32_t smask;
   uint32_t sshift;
-  uint32_t ways;
-  uint32_t wshift;
   uint32_t ssize;
-  uint32_t set;
-  uint32_t sw;
 
   /* Get the characteristics of the D-Cache */
 
   ccsidr = getreg32(NVIC_CCSIDR);
-  smask  = CCSIDR_SETS(ccsidr);          /* (Number of sets) - 1 */
   sshift = CCSIDR_LSSHIFT(ccsidr) + 4;   /* log2(cache-line-size-in-bytes) */
-  ways   = CCSIDR_WAYS(ccsidr);          /* (Number of ways) - 1 */
 
-  /* Calculate the bit offset for the way field in the DCISW register by
-   * counting the number of leading zeroes.  For example:
-   *
-   *   Number of  Value of ways  Field
-   *   Ways       'ways'         Offset
-   *     2         1             31
-   *     4         3             30
-   *     8         7             29
-   *   ...
-   */
-
-  wshift = arm_clz(ways) & 0x1f;
-
-  /* Invalidate the D-Cache over the range of addresses */
+  /* Invalidate the D-Cache containing this range of addresses */
 
   ssize  = (1 << sshift);
+
+  /* Round down the start address to the nearest cache line boundary.
+   *
+   *   sshift = 5      : Offset to the beginning of the set field
+   *   (ssize - 1)  = 0x007f : Mask of the set field
+   */
+
   start &= ~(ssize - 1);
   ARM_DSB();
 
   do
     {
-      int32_t tmpways = ways;
-
-      /* Isolate the cache line associated with this address.  For example
-       * if the cache line size is 32 bytes and the cache size is 16KB, then
-       *
-       *   sshift = 5      : Offset to the beginning of the set field
-       *   smask  = 0x007f : Mask of the set field
+      /* The below store causes the cache to check its directory and
+       * determine if this address is contained in the cache. If so, it
+       * invalidate that cache line. Only the cache way containing the
+       * address is invalidated. If the address is not in the cache, then
+       * nothing is invalidated.
        */
 
-      set = ((uint32_t)start >> sshift) & smask;
-
-      /* Clean and invalidate each way for this cacheline */
-
-      do
-        {
-          sw = ((tmpways << wshift) | (set << sshift));
-          putreg32(sw, NVIC_DCISW);
-        }
-      while (tmpways--);
+      putreg32(start, NVIC_DCIMVAC);
 
       /* Increment the address by the size of one cache line. */
 
